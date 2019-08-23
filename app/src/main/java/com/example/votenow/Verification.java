62e931c263d846e90b898c706cc9ef6860f9389b
Sayantan Ghosh <gsayantan01@gmail.com>
2019-08-23 15:43:57 +0530

    Upload Ui

--- app/src/main/java/com/example/votenow/Verification.java
+++ app/src/main/java/com/example/votenow/Verification.java
@@ -1,13 +1,41 @@
 package com.example.votenow;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Verification extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1;
+    public Button faceButton,idButton;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+        faceButton=findViewById(R.id.inputFace);
+        idButton=findViewById(R.id.inputFace);
+
+        faceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(cameraIntent.resolveActivity(getPackageManager())!=null){
+                    startActivityForResult(cameraIntent,CAMERA_REQUEST);
+                }
+            }
+        });
+    }
+
+    private void captureImage() {
     }
 }
