03258bebc34a88b0e866d178a337a22bc49c3ac4
Anubhab Mukherjee <anubhab.mukherjee2013@gmail.com>
2019-08-23 23:36:35 +0530

    Base64 encoded

--- app/src/main/java/com/example/votenow/Verification.java
+++ app/src/main/java/com/example/votenow/Verification.java
@@ -12,6 +12,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,6 +25,11 @@ import com.vansuita.pickimage.listeners.IPickClick;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -64,7 +71,7 @@ public class Verification extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent,0);
+                    startActivityForResult(cameraIntent,456);
             }
         });
 
@@ -79,21 +86,6 @@ public class Verification extends AppCompatActivity {
         }else{
             idButton.setError("Id Not Uploaded");
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            bitmap=(Bitmap)data.getExtras().get("data");
-            circleImageView.setImageBitmap(bitmap);
-            circleImageView.setVisibility(View.VISIBLE);
-            faceButton.setHint("Change Face?");
-        }catch (Exception e){
-            Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
-        }
-
-
     }
 
 
@@ -133,6 +125,7 @@ public class Verification extends AppCompatActivity {
                 .show(this);
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         try {
             pickImageDialog = null; //.dismiss();
         }catch(Exception e){}
@@ -141,25 +134,64 @@ public class Verification extends AppCompatActivity {
             case 753:
                 if(resultCode == RESULT_OK) {
                     Uri selectedImage = data.getData();
-                    ((ImageView) findViewById(R.id.imageView)).setImageURI(selectedImage);
+                    //((ImageView) findViewById(R.id.imageView)).setImageURI(selectedImage);
+                    try {
+                        InputStream iStream =   getContentResolver().openInputStream(selectedImage);
+                        byte[] inputData = getBytes(iStream);
+                        String image = new String(Base64.encode(inputData, Base64.DEFAULT));
+                        //Toast.makeText(this, image, Toast.LENGTH_LONG).show();
+                        Log.v("ImageData", image);
+                    } catch (Exception e){}
                 }
                 break;
             case 951:
                 if (resultCode == RESULT_OK && data.hasExtra("data")) {
                     Bitmap bitmap = (Bitmap) data.getExtras().get("data");
                     if (bitmap != null) {
-                        ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+                        //((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+                        try {
+                            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                            byte[] imagedata = stream.toByteArray();
+                            bitmap.recycle();
+                            String image = new String(Base64.encode(imagedata, Base64.DEFAULT));
+                            //Toast.makeText(this, image, Toast.LENGTH_LONG).show();
+                            Log.v("ImageData", image);
+                        } catch (Exception e){}
                     }
-
                 }
                 break;
+            case 456:
+                try {
+                    bitmap=(Bitmap)data.getExtras().get("data");
+                    circleImageView.setImageBitmap(bitmap);
+                    circleImageView.setVisibility(View.VISIBLE);
+                    faceButton.setHint("Change Face?");
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    byte[] imagedata = stream.toByteArray();
+                    bitmap.recycle();
+                    String image = new String(Base64.encode(imagedata, Base64.DEFAULT));
+                    //Toast.makeText(this, image, Toast.LENGTH_LONG).show();
+                    Log.v("ImageData", image);
+                }catch (Exception e){
+                    Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
             default:
                 super.onActivityResult(requestCode, resultCode, data);
                 break;
-		5A
-		5A
-		5A
-		5A
         }
+    }
+
+    public byte[] getBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
     }
 }
